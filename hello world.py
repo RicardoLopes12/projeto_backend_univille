6c83b44bb0cc349dcce3f5eb21149daaeafa9651
gustavoSantos0 <gu.dossantos0@gmail.com>
2025-10-30 21:05:51 -0300

    mudei uns negocios

--- hello world.py
+++ hello world.py
@@ -1,13 +1,34 @@
-import datetime
-import logging
+# main.py
+from fastapi import FastAPI
+import requests
+from fastapi.middleware.cors import CORSMiddleware
 
-logging.basicConfig(level=logging.INFO)
+app = FastAPI()
 
-def hello(name="World"):
-    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    message = f"👋 Hello, {name}! Time now: {now}"
-    logging.info(message)
-    return message
+# Permitir que qualquer front-end acesse a API
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-if __name__ == "__main__":
-    hello()
+@app.get("/clima")
+def clima():
+    
+    loc = requests.get("https://ipwho.is").json()
+    lat = loc["latitude"]
+    lon = loc["longitude"]
+    cidade = loc["city"]
+
+  
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m"
+    resp = requests.get(url).json()
+    temperatura_agora = resp["hourly"]["temperature_2m"][0]
+
+    return {
+        "cidade": cidade,
+        "latitude": lat,
+        "longitude": lon,
+        "temperatura_agora": temperatura_agora
+    }
